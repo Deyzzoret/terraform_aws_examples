0d5373b205e1556c581ace5a456c4390d0ef3d28
Julio Cesar Camargo Amaro <JulioCesar.CAMARGOAMARO@3ds.com>
2023-08-09 15:49:41 +0200

    Comments on ECS cluster

--- ECR-ECS-ELB-VPC-ECS-Service/ecs.tf
+++ ECR-ECS-ELB-VPC-ECS-Service/ecs.tf
@@ -1,18 +1,36 @@
 # Includes ECS Cluster, Task Definition, Role and Policy code
 
 # Getting data existed ECR
+# Data source bloc, used to retrieve information related to 
+# ECR created for application
 data "aws_ecr_repository" "flask_app_tf" {
   name = "flask-app-ecr"
 }
 
 # Creating ECS Cluster
+# The aws_ecs_cluster resource in Terraform is used to create an Amazon ECS cluster, which is a logical grouping of tasks and services.
+# An ECS cluster represents a pool of resources (such as EC2 instances or Fargate tasks) that can be used to run and manage your containerized applications.
+# From my understanding, this is where it will be contained all my
+# container services running in AWS FARGATE, or at least where they will
+# be deployed.
+
+# The aws_ecs_cluster resource in Terraform is used to create an Amazon ECS cluster,
+# which is a logical grouping of tasks and services. An ECS cluster represents a pool
+# of resources (such as EC2 instances or Fargate tasks) that can be used to run and
+# manage your containerized applications.
+
+# In summary, the aws_ecs_cluster resource is used to create a cluster that serves as a foundational
+# environment for running containerized applications. You can definitely associate multiple services
+# with a single cluster to efficiently manage and scale your container workloads.
 resource "aws_ecs_cluster" "flask_app_ecs_cluster_tf" {
   # Naming the cluster
   name = "flask_app_ecs_cluster" 
 }
 
 # Creating ECS Task
-# TODO: Check what is this 
+# This represents the lowest running entity in Fargate or EC2 instance with ECS.
+# Normally, we will have to to define a Task template within it will be defined
+# the spec of the image to execute either on Fargate/EC2-ECS.
 resource "aws_ecs_task_definition" "flask_app_task_tf" {
   family                   = "flask-app-task" 
   container_definitions    = <<DEFINITION
@@ -43,6 +61,8 @@ resource "aws_ecs_task_definition" "flask_app_task_tf" {
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
+# This is like if we were defining the assume role policy inside the 
+# IAM role definition. Like if we were saving content on a variable.
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -64,5 +84,6 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
 # Role - Policy Attachment for ECS
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  // Policy defined by AWS
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
